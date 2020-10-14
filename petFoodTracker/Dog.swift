9bc9c9e5d931757a8ae33d86c761cb99740b29ce
Haruko <haruko.flipsy.krb@gmail.com>
2020-10-14 22:14:23 +0900

    firebase functions

---
+++ petFoodTracker/Dog.swift
@@ -0,0 +1,9 @@
+//
+//  Dog.swift
+//  petFoodTracker
+//
+//  Created by Haruko Okada on 10/11/20.
+//  Copyright © 2020 Haruko Okada. All rights reserved.
+//
+
+import Foundation
---
+++ petFoodTracker/DogTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DogTableViewCell.swift
+//  petFoodTracker
+//
+//  Created by Haruko Okada on 10/11/20.
+//  Copyright © 2020 Haruko Okada. All rights reserved.
+//
+
+import UIKit
+
+class DogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
