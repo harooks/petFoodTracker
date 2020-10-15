bf70caf809b00c1104ffaa665ba81d9cc4ad0269
Haruko <haruko.flipsy.krb@gmail.com>
2020-10-15 21:41:49 +0900

    updated firebaseFunctions

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -3,39 +3,34 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.sendNotificationToTopic = functions.firestore.document('users/{uid}/{didFeedPet}').onWrite(async (change, context) => {
+exports.sendNotificationToTopic = functions.firestore.document('users/{userId}').onUpdate(async (change, context) => {
 
-    const newValue = snapshot.data()
+    const previousData = change.before.data();
     const data = change.after.data();
-    //const previousData = change.before.data();
 
-         // uidから通知先のユーザー情報を取得
-        const userRef = await admin
-        .firestore()
-        .collection("users")
-        .doc(context.params.userId);
-      const userDoc = await userRef.get();
-
-if (userDoc.exists) {
-const user = userDoc.data();
-const message = {
-    notification: {
-        title: "ごはんありがとう",
-        body: "ペットにごはんが与えられました"
-    }
-};
-      /// プッシュ通知を送信
-      if (user.fcmToken) {
-        admin.messaging().sendToDevice(user.fcmToken, message);
-      } else {
-        console.error("No Firebase Cloud Messaging Token.");
-      }
-    } else {
-        console.error("No User.");
-      }
+    if (userDoc.exists) {
+        const user = userDoc.data();
+        const message = {
+            notification: {
+                title: "ごはんありがとう",
+                body: "ペットにごはんが与えられました"
+            }
+        };
+        
+        /// Field "didFeedPet" が false から true になった時をトリガーとする
+        if (data.didFeedPet === 1 && previousData.didFeedPet === 0) {
+            /// プッシュ通知を送信
+            if (user.fcmToken) {
+                admin.messaging().sendToDevice(user.fcmToken, message);
+            } else {
+                console.error("No Firebase Cloud Messaging Token.");
+            }
+        } else {
+            console.error("No User.");
+        }
       
-      return true;
-
+        return true;
+    }
 });
 
 
